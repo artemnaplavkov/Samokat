c12e38c0943779e51a43b9cb07e35f718212ff1e
artemnaplavkov <74201877+artemnaplavkov@users.noreply.github.com>
2024-05-18 12:25:52 +0300

    Time shift

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ main.cpp courier.cpp database_manager.cpp order.cpp point.cpp point_manager.cpp storage.cpp time_manager.cpp -o samokat
--- time_manager.h
+++ time_manager.h
@@ -8,4 +8,17 @@ struct TimeManager{
 		std::vector<abstracts::Storage>& storages,
 		std::vector<abstracts::Courier>& couriers,
 		std::vector<abstracts::Order>& orders);
+private:
+	void assign_orders(
+		std::vector<abstracts::Storage>& storages,
+		std::vector<abstracts::Courier>& couriers,
+		std::vector<abstracts::Order>& orders);
+	void one_hour_move(
+		std::vector<abstracts::Storage>& storages,
+		std::vector<abstracts::Courier>& couriers,
+		std::vector<abstracts::Order>& orders);
+	void complete_orders(
+		std::vector<abstracts::Storage>& storages,
+		std::vector<abstracts::Courier>& couriers,
+		std::vector<abstracts::Order>& orders);
 };
