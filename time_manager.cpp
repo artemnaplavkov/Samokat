15d7e7a27d2ad50fc8f2b31fbd5b276dd385dae2
Plaksina-Ekaterina <plaksinaekaterina04@yandex.ru>
2024-05-22 09:55:46 +0300

    add time manager

--- time_manager.cpp
+++ time_manager.cpp
@@ -1,9 +1,20 @@
 #include "time_manager.h"
+#include <iostream>
 
-
-void TimeManager::time_shift(int hours,
-	std::vector<abstracts::Storage>& storages,
-	std::vector<abstracts::Courier>& couriers,
-	std::vector<abstracts::Order>& orders) {
-
+void TimeManager::time_shift(float hours,
+	std::vector<abstracts::Storage>* storages,
+	std::vector<abstracts::Courier>* couriers,
+	std::vector<abstracts::Order>* orders) {
+	for (int i = 0; i < orders->size(); i++) {
+		(*orders)[i].time_gone(hours);
+		if ((*orders)[i].get_time() <= 0) {
+			AbstractManagers::Order_Manager::SetCondition((*orders)[i], 2);
+			for (int j = 0; j < couriers->size(); j++) {
+				if ((*couriers)[j].current_order_id == (*orders)[i].ID()) {
+					(*couriers)[j].set_order(-1);
+					break;
+				}
+			}
+		}
+	}
 }
--- time_manager.h
+++ time_manager.h
@@ -2,10 +2,12 @@
 #include "courier.h"
 #include "order.h"
 #include "storage.h"
+#include "order_manager.h"
 #include <vector>
+
 struct TimeManager{
-	void time_shift(int hours,
-		std::vector<abstracts::Storage>& storages,
-		std::vector<abstracts::Courier>& couriers,
-		std::vector<abstracts::Order>& orders);
+	void time_shift(float hours,
+		std::vector<abstracts::Storage>* storages,
+		std::vector<abstracts::Courier>* couriers,
+		std::vector<abstracts::Order>* orders);
 };
